0c193e805150b47474e6ab59b3b8558c839f480f
Alan Maxwell <alanmaxwellantunes@gmail.com>
2021-05-16 12:55:57 -0300

    chore: add function to separate letters from image

--- captcha_treatment/captcha_treatment.py
+++ captcha_treatment/captcha_treatment.py
@@ -30,3 +30,47 @@ def treat_images(origin_folder, destiny_folder='images/treated_images'):
 
         filename = os.path.basename(file)
         copy_image.save(f'{destiny_folder}/{filename}')
+
+
+def separate_letters():
+    files = glob.glob('images/treated_images/*')
+    for file in files:
+        image = cv2.imread(file)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Convert image to binary
+        _, binary_image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV)
+
+        # Find contours of each letter
+        contours, _ = cv2.findContours(
+            binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        letters_area = []
+
+        # Filter contours that are letters
+        for contour in contours:
+            (x, y, width, height) = cv2.boundingRect(contour)
+            area = cv2.contourArea(contour)
+
+            if area > 115:
+                letters_area.append((x, y, width, height))
+
+        if len(letters_area) != 5:
+            continue
+
+        # Draw countours and separate the letters in individual files
+        final_image = cv2.merge([image] * 3)
+
+        index = 0
+        for rectangle in letters_area:
+            x, y, width, height = rectangle
+            letter_image = image[y-2:y+height+2, x-2:x+width+2]
+            index += 1
+            filename = os.path.basename(file).replace(
+                ".png", f"letra{index}.png")
+            cv2.imwrite(f'images/letters/{filename}', letter_image)
+            cv2.rectangle(final_image, (x-2, y-2),
+                          (x+width+2, y+height+2), (0, 255, 0), 1)
+
+        filename = os.path.basename(file)
+        cv2.imwrite(f"images/identified/{filename}", final_image)
